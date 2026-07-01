530c77e28694aea02fffc9998589b151d745b935
wontae <83wontae@gmail.com>
2026-07-01 14:22:10 +0900

    32143

--- Source/ProjectRTS/ProjectRTS.Build.cs
+++ Source/ProjectRTS/ProjectRTS.Build.cs
@@ -6,18 +6,24 @@ public class ProjectRTS : ModuleRules
 {
 	public ProjectRTS(ReadOnlyTargetRules Target) : base(Target)
 	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "AIModule", "NavigationSystem" });
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        // 1. 유니티 빌드 활성화 (명령행 길이 단축에 필수)
+        bUseUnity = true;
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        // 2. 외부 인클루드 경로 주입 최소화
+        // 엔진 모듈은 PublicDependencyModuleNames를 통해서만 참조해야 합니다.
+        PublicDependencyModuleNames.AddRange(new string[] {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "UMG",
+            "AIModule",
+            "NavigationSystem"
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+    }
 }
